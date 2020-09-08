d40054aba7e14908da8d42370f7550830e347e3d
Aaryan Goel <apg5729@psu.edu>
2020-09-08 18:47:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+
+
+
+
+numGrade = float(input("Enter your CMPSC 131 grade: "))
+
+
+if numGrade >= 93:
+  {
+    print("Your letter grade for CMPSC 131 is A.")
+  }
+elif numGrade >=90:
+  {
+    print("Your letter grade for CMPSC 131 is A-.")
+  }
+elif numGrade >=87:
+  {
+    print("Your letter grade for CMPSC 131 is B+.")
+  }
+elif numGrade >=83:
+  {
+
+    print("Your letter grade for CMPSC 131 is B.")
+  }
+elif numGrade >=80:
+  {
+    print("Your letter grade for CMPSC 131 is B-.")
+  }
+elif numGrade >=77:
+  {
+    print("Your letter grade for CMPSC 131 is C+.")
+  }
+elif numGrade >=60:
+  {
+    print("Your letter grade for CMPSC 131 is D.")
+  }
+elif numGrade < 60:
+  {
+    print("Your letter grade for CMPSC 131 is F.")
+
+  }
